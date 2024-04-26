ee37bee0a30da779626306a36f40c0dabed586c6
Hood <mariusz.budzynski87@gmail.com>
2024-04-26 11:32:04 +0200

    Add project files.

---
+++ IdentityApp/GlobalUsings.cs
@@ -0,0 +1,6 @@
+﻿global using IdentityApp.Areas.Identity;
+global using IdentityApp.Data;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.EntityFrameworkCore;
+global using IdentityApp;
---
+++ IdentityApp/ServicesRegistration.cs
@@ -0,0 +1,20 @@
+﻿namespace IdentityApp
+{
+    public static class ServicesRegistration
+    {
+        public static void RegisterServices(this WebApplicationBuilder builder)
+        {
+            // Add services to the container.
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(connectionString));
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+            builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
+            builder.Services.AddSingleton<WeatherForecastService>();
+        }
+    }
+}

2080af47fc2e7daee6663e537e1e50aa476b8227
Hood <mariusz.budzynski87@gmail.com>
2024-04-26 12:56:06 +0200

    Fixed configuration issues

--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -1,14 +1,4 @@
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using IdentityApp.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 ServicesRegistration.RegisterServices(builder);
 
--- IdentityApp/ServicesRegistration.cs
+++ IdentityApp/ServicesRegistration.cs
@@ -5,12 +5,14 @@
         public static void RegisterServices(this WebApplicationBuilder builder)
         {
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection");
+            var connectionString = builder.Configuration.GetConnectionString("ApplicationDbContextConnection") ?? throw new InvalidOperationException("Connection string 'ApplicationDbContextConnection' not found.");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
-            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDatabaseDeveloperPageExceptionFilter();
             builder.Services.AddRazorPages();
             builder.Services.AddServerSideBlazor();
             builder.Services.AddScoped<AuthenticationStateProvider, RevalidatingIdentityAuthenticationStateProvider<IdentityUser>>();
